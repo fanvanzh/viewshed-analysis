c7ae498c45cc7d5cfa8aab2443e414c7d04ee046
fanzhenhua <fanzhenhua@superengine.com.cn>
2018-06-15 17:45:17 +0800

    add viewed outline to express the viewed range

--- test.js
+++ test.js
@@ -37,25 +37,54 @@ function init() {
         cube.position.set( (Math.random() - 0.5)*30, height / 2, (Math.random() - 0.5)*30);
         scene.add(cube);
     }
+    // 视点
     let geometry = new THREE.SphereGeometry( 0.3, 32, 32 );
     material = new THREE.MeshBasicMaterial( {color: 0xff0000} );
     let sphere = new THREE.Mesh( geometry, material );
-    sphere.position.set(0,5,0);
+    let eye_height = 6;
+    sphere.position.set(0,eye_height,0);
     scene.add( sphere );
-    
+    // 轮廓线
+    var material_line = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+    {
+        // 
+        let view_range = 15;
+        geometry = new THREE.Geometry();
+        geometry.vertices.push(new THREE.Vector3( 0, eye_height, 0) );
+        geometry.vertices.push(new THREE.Vector3( -10, eye_height + 5, view_range) );
+        geometry.vertices.push(new THREE.Vector3( -10, eye_height - 5, view_range) );
+        
+        geometry.vertices.push(new THREE.Vector3( 0, eye_height, 0) );
+        geometry.vertices.push(new THREE.Vector3( 10, eye_height + 5, view_range) );
+        geometry.vertices.push(new THREE.Vector3( 10, eye_height - 5, view_range) );
+        geometry.vertices.push(new THREE.Vector3( 0, eye_height, 0) );
+
+        var line = new THREE.Line( geometry, material_line );
+        scene.add( line );
+        
+        geometry = new THREE.Geometry();
+        geometry.vertices.push(new THREE.Vector3( -10, eye_height + 5, view_range) );
+        geometry.vertices.push(new THREE.Vector3( -10, eye_height - 5, view_range) );
+        geometry.vertices.push(new THREE.Vector3( 10, eye_height - 5, view_range) );
+        geometry.vertices.push(new THREE.Vector3( 10, eye_height + 5, view_range) );
+        geometry.vertices.push(new THREE.Vector3( -10, eye_height + 5, view_range) );
+        line = new THREE.Line( geometry, material_line );
+        scene.add( line );
+    }
+
     var plane = new THREE.Plane( new THREE.Vector3( 0, 1, 0 ), 0 );
     var helper = new THREE.PlaneHelper( plane, 30, 0x000000 );
     scene.add( helper );
 
-    var axesHelper = new THREE.AxesHelper( 40 );
+    var axesHelper = new THREE.AxesHelper( 15 );
     scene.add( axesHelper );
 }
 
 function animate() {
     requestAnimationFrame( animate );
     let step = 0.001;
-    let range = 25;
-    camera.position.set(range * Math.sin(ticks * step), range - 10, range * Math.cos(ticks * step));
+    let range = 35;
+    camera.position.set(range * Math.sin(ticks * step), 15, range * Math.cos(ticks * step));
     camera.lookAt(new THREE.Vector3(0, 0, 0));
     ticks += 1;
     renderer.render( scene, camera );
